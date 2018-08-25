a2512e8f40e7a7e76e549585d2e8acf6f35b35a9
catchdebanjan <41503360+catchdebanjan@users.noreply.github.com>
2018-08-25 19:36:05 +0530

    wer

--- src/routes/services.js
+++ src/routes/services.js
@@ -3,19 +3,19 @@ const router=express.Router();
 
 const { accounts, writeJSON } = require('../data');
 
-app.get('/transfer', (req, res) => res.render('transfer'));
+router.get('/transfer', (req, res) => res.render('transfer'));
 
-app.post('/transfer', (req, res) => {
+router.post('/transfer', (req, res) => {
   accounts["savings"].balance = accounts["savings"].balance - req.body.amount;
   accounts["checking"].balance = parseInt(accounts["checking"].balance) + parseInt(req.body.amount,10);
   writeJSON();
   res.render('transfer', {message:'Transfer Completed'});
 });
 
-app.get('/payment', (req, res) => {res.render('payment', {account: accounts.credit});
+router.get('/payment', (req, res) => {res.render('payment', {account: accounts.credit});
 });
 
-app.post('/payment', (req, res) => {
+router.post('/payment', (req, res) => {
   accounts.credit.balance-=req.body.amount;
   accounts.credit.available+=parseInt(req.body.amount,10);
   writeJSON();
